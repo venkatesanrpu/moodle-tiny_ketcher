e5425763db0cd47817a0e8a0979a057d4836613f
venkatesanrpu <registerme1963@gmail.com>
2025-07-26 09:43:32 +0530

    Create tiny_ketcher.php

---
+++ lang/en/tiny_ketcher.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     tiny_ketcher
+ * @category    string
+ * @copyright   2024 Venkatesan Rangarajan <venkatesanrpu@gmail.com>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Ketcher Editor';
+$string['pluginName'] = 'Ketcher Editor';
+$string['privacy:metadata'] = 'Chemical Structure Editor does not store any personal data';
+$string['sketchtitle'] = 'Ketcher Editor';
+$string['buttonNameTitle'] = 'Ketcher Editor';
